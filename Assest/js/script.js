608182f7b9f112e783b65eada4c0d809dc55266e
JANAKALIDASAN T <104510427+Janakalidasan@users.noreply.github.com>
2023-03-16 17:03:43 +0530

    Demo08

---
+++ Assest/js/script.js
@@ -0,0 +1,53 @@
+$(function () {
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 50) {
+            $(".navbar").removeClass("navbar-bg");
+            $(".navbar").addClass("navbar-bg2").style.top = "0";
+        } else {
+            $(".navbar").removeClass("navbar-bg2");
+            $(".navbar").addClass("navbar-bg").style.top = "-50px";
+        }
+    });
+});
+fetch("./header.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("header").innerHTML = data;
+    });
+
+fetch("./footer.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("footer").innerHTML = data;
+    });
+//
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+      margin:10,
+      nav:true,
+      autoplay:true,
+      autoplayTimeout:3000,
+      autoplayHoverPause:true,
+      center: true,
+      navText: [
+          "<i class='fa fa-angle-left'></i>",
+          "<i class='fa fa-angle-right'></i>"
+      ],
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:3
+          }
+      }
+    });
+  });
